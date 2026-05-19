aeb5c553d8171210bc0ca78fcc8dc5d50e374071
agruet <agruet@IoT.myguest.virtualbox.org>
2026-05-19 18:52:05 +0200

    p3 started

---
+++ p3/scripts/install.sh
@@ -0,0 +1,32 @@
+# 1. Install Docker
+
+## Detect OS
+. /etc/os-release
+DISTRO_ID="${ID}"
+
+## Add Docker's official GPG key:
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL "https://download.docker.com/linux/${DISTRO_ID}/gpg" -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+## Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/${DISTRO_ID}
+Suites: $(. /etc/os-release && echo "${VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# 2. Install k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# 3. Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl && sudo mv kubectl /usr/local/bin/
---
+++ p3/scripts/launch.sh
@@ -0,0 +1,1 @@
+k3d cluster create --config p3/confs/k3d-config.yaml
