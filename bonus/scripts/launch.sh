c2a27c2d5bbf652d52f778727f532ae6a63157d9
Anicet78 <anicet.gruet@gmail.com>
2026-06-12 14:35:00 +0200

    Setup hostname

--- bonus/scripts/launch.sh
+++ bonus/scripts/launch.sh
@@ -1,1 +1,6 @@
 k3d cluster create --config bonus/confs/k3d-config.yaml
+
+if [ $(cat /etc/hosts | grep gitlab | wc -l) -eq 0 ]
+then
+    echo 127.0.0.1 gitlab.bonus.com | sudo tee -a /etc/hosts > /dev/null
+fi
