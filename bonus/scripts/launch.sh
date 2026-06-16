43d2254731e6e1c3a44dd47d57488ebed5f74aa9
Anicet78 <anicet.gruet@gmail.com>
2026-06-16 12:42:01 +0200

    fix: remove script

--- bonus/scripts/launch.sh
+++ bonus/scripts/launch.sh
@@ -2,14 +2,9 @@
 
 REPO_PATH=$(pwd)
 
-k3d cluster create --config bonus/confs/k3d-config.yaml --volume $REPO_PATH/bonus:/app@all
+k3d cluster create --config bonus/confs/k3d-config.yaml
 
 if [ $(cat /etc/hosts | grep gitlab | wc -l) -eq 0 ]
 then
     echo 127.0.0.1 gitlab.bonus.com | sudo tee -a /etc/hosts > /dev/null
-fi
-
-if [ $(git remote -v | grep gitlab.bonus.com | wc -l) -eq 0 ]
-then
-    git remote add gitlab git@gitlab.bonus.com:root/iot.git
 fi
