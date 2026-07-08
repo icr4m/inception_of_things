663d7b707cf480e5762d81ff2edc6b4899f46deb
bader <badzaidi550@gmail.com>
2026-07-08 23:28:53 +0200

    feat: added vagrantfile and ingress

--- p2/scripts/start.sh
+++ p2/scripts/start.sh
@@ -1,1 +1,10 @@
-sudo kubectl apply -f ../config
+sudo kubectl apply -f ../config/jellyfin/deployement.yaml
+sudo kubectl apply -f ../config/jellyfin/service.yaml
+
+sudo kubectl apply -f ../config/plex/deployement.yaml
+sudo kubectl apply -f ../config/plex/service.yaml
+
+sudo kubectl apply -f ../config/nginx/deployement.yaml
+sudo kubectl apply -f ../config/nginx/service.yaml
+
+sudo kubectl apply -f ../config/ingress.yaml
